d8796c83adf2f2636314bdb282b58d3baa41db42
Ruslan Karabalin <karabalin112@gmail.com>
2025-11-30 00:16:13 +0300

    feat: add exercise 1-9

---
+++ exercise109/main.cpp
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+// Exercise: Write a program to copy its input to its output,
+// replacing each string of one or more blanks by single blank
+int main() {
+    int c;
+    bool isFirst = true;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ') {
+            printf("%c", c);
+            if (!isFirst) {
+                isFirst = true;
+            }
+        } else {
+            if (isFirst) {
+                isFirst = false;
+                printf("%c", c);
+            }
+        }
+    }
+    return 0;
+}
