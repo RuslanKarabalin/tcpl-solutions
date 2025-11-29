9f94a584f89545a762bb79c70220102d6fc74451
Ruslan Karabalin <karabalin112@gmail.com>
2025-11-30 00:04:02 +0300

    feat: add exercise 1-8

---
+++ exercise18/main.cpp
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+// Exercise: Write a program to count
+// blanks, tabs, and newlines
+int main() {
+    int c;
+    int bc = 0;
+    int tc = 0;
+    int nc = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            ++bc;
+        } else if (c == '\t') {
+            ++tc;
+        } else if (c == '\n') {
+            ++nc;
+        }
+    }
+    printf("Blanks count: %d\n", bc);
+    printf("Tabs count: %d\n", tc);
+    printf("Newlines count: %d\n", nc);
+    return 0;
+}
